fcd6d786c1b36170d15e0cdefe3504f1b43ae309
T4kano <54607722+T4kano@users.noreply.github.com>
2021-07-05 15:07:40 -0300

    Page PIX

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,4 @@
-let scene = document.querySelector(".scene");
-let parallax = new Parallax(scene);
+/*
 window.sr = ScrollReveal(
     {
         delay: 200,
@@ -8,6 +7,7 @@ window.sr = ScrollReveal(
         distance: "-25%"
     }
 );
+
 sr.reveal("#about-title");
 sr.reveal(".about", { delay: 400 });
 sr.reveal(".about-img", { delay: 400, origin: "left" });
@@ -24,14 +24,4 @@ sr.reveal("#partner-2");
 sr.reveal("#partner-3", { delay: 600 });
 sr.reveal("#partner-4", { delay: 400 });
 sr.reveal("#partner-img", { origin: "left" });
-function initMap() {
-    var location = { lat: -23.085076844458957, lng: -47.225054519829676 };
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: location
-    });
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map
-    });
-}
+*/
