63a57f8c2e0dc57a0871006fb986ee84bae2afe9
T4kano <54607722+T4kano@users.noreply.github.com>
2021-07-05 10:55:01 -0300

    Primeiro Commit

---
+++ dist/index-coming-soon.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Em Breve | LTW Bank - O Banco 100% Digital - Abra sua conta</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Em breve os aplicativos LTW Bank estão disponíveis, deixe o seu contato!">
+	<meta name="author" content="Juliana Diani">
+	
+	<?php  include_once('includes/head.php')?>
+
+</head>
+<body>
+	<div class="content-wrapper" style="background: #fff">
+		
+		<div class="wrapper white-wrapper pt-50 mt-70" style="padding-bottom: 0; margin-bottom: 0;">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-2"></div>
+					
+					<div class="col-md-8" style="text-align: center">
+					   <img src="images/logo-ltw.png" alt="" style="width: 200px; padding: 11px 10px; margin-bottom: 50px;" />
+					   <br>
+						<h2 class="section-title">ESTÁ CHEGANDO O<br> BANCO DA SUA VITÓRIA </h2>
+						<p class="fs-home-2" style="font-size: 23px">Esse site está em manutenção!</p>
+					</div>
+					
+					<div class="col-md-3 mobile-mt-50">
+						<!--<img src="images/app-mockup-home2.png" alt="Aplicativo da LTW Bank" class="w-100" />-->
+					</div>
+					
+					<div class="col-md-2"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+		
+
+		<script type="text/javascript" src="js/jquery.min.js"></script> 
+		<script type="text/javascript" src="js/bootstrap.min.js"></script> 
+		<script type="text/javascript" src="js/plugins.js"></script> 
+		<script type="text/javascript" src="js/scripts.js"></script>
+		<script type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js"></script> 
+		<script type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js"></script>
+</body>
+</html>
