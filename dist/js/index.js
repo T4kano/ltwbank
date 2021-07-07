18ea775e6fabbbf9f36765717491cb9e0c142b81
T4kano <54607722+T4kano@users.noreply.github.com>
2021-07-07 11:11:16 -0300

    Performance

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,29 +1,5 @@
-console.log("JS");
+// LOAD
+$(window).on("load", function() {
+    $(".loading").fadeOut(1000);
 
-/*
-window.sr = ScrollReveal(
-    {
-        delay: 200,
-        origin: "right",
-        duration: 2000,
-        distance: "-25%"
-    }
-);
-
-sr.reveal("#about-title");
-sr.reveal(".about", { delay: 400 });
-sr.reveal(".about-img", { delay: 400, origin: "left" });
-sr.reveal("#stripe", { delay: 600 });
-sr.reveal("#card-1", { origin: "top" });
-sr.reveal("#card-2", { origin: "top", delay: 400 });
-sr.reveal("#card-3", { origin: "top", delay: 600 });
-sr.reveal("#card-4", { origin: "top", delay: 800 });
-sr.reveal("#value-1");
-sr.reveal("#value-2", { delay: 400 });
-sr.reveal("#value-3", { delay: 600 });
-sr.reveal("#partner-1", { delay: 400 });
-sr.reveal("#partner-2");
-sr.reveal("#partner-3", { delay: 600 });
-sr.reveal("#partner-4", { delay: 400 });
-sr.reveal("#partner-img", { origin: "left" });
-*/
+});
