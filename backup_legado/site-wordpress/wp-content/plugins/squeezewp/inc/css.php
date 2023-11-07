5e74de130fa875087087a4c5e4c2bb0476e656f6
T4kano <edutakajr@gmail.com>
2023-11-07 15:50:02 -0300

    Takano LTW Bank 2023

--- backup_legado/site-wordpress/wp-content/plugins/squeezewp/inc/css.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if (get_option('optionverification') == 200){
-    	$css = get_option('scripts_squeeze_wp');
-    	$css .= get_field('css');
-?>
-<style>
-    <?php echo $css; ?>
-</style>
-    <?php } ?>
--- backup_legado/site-wordpress/wp-content/plugins/squeezewp/inc/scripts.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $scripts = get_option('scripts_squeeze_wp');
-    echo stripslashes($scripts);
-    $scripts_page = get_field('scripts');
-    echo htmlspecialchars_decode(stripslashes($scripts_page));
-?>
